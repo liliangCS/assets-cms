ba6e8204268872a2153608f116fcfa66fff94acc
lilian <1220795758@qq.com>
2022-09-13 12:10:58 +0800

    220913完成pc端、移动端布局

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,12 +6,15 @@ export default function Home() {
   return (
     <div className={style.home}>
       {/* 侧边栏 */}
-      <div className={style.aside}>
-        侧边栏
-      </div>
+      <div className={style.aside}>侧边栏</div>
       {/* 主体内容区 */}
       <div className={style.main}>
-        主体内容区
+        {/* 头部区域 */}
+        <div className={style.header}>头部区域</div>
+        {/* 移动端tab栏（屏幕宽度800px之下显示） */}
+        <div className={style.tab}>tab栏</div>
+        {/* 主体内容区 */}
+        <div className={style.content}>主体内容区</div>
       </div>
     </div>
   )
