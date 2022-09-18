fcc5944a670e1aec0da03bc74caae3a1ce9e5a2a
lilian <1220795758@qq.com>
2022-09-18 12:18:09 +0800

    0918检索组件修订

--- src/pages/Assets/Search/index.tsx
+++ src/pages/Assets/Search/index.tsx
@@ -4,12 +4,9 @@ import style from "./style.module.scss"
 export default function Search() {
   return (
     <div className={style.search}>
-      <div className={style.input}>
-        <input type="text" /><button>搜索</button>
-      </div>
-      <div className={style.addImg}>
-        <button>添加图片</button>  
-      </div>
+        <div className={style.btn}>
+          添加图片
+        </div>
     </div>
   )
 }
