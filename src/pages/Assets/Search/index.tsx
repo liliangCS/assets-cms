cf8055802fe48d85ac28c17c8fedf867a7632573
lilian <1220795758@qq.com>
2022-09-18 11:37:42 +0800

    图片检索功能

--- src/pages/Assets/Search/index.tsx
+++ src/pages/Assets/Search/index.tsx
@@ -4,7 +4,12 @@ import style from "./style.module.scss"
 export default function Search() {
   return (
     <div className={style.search}>
-      图片检索
+      <div className={style.input}>
+        <input type="text" /><button>搜索</button>
+      </div>
+      <div className={style.addImg}>
+        <button>添加图片</button>  
+      </div>
     </div>
   )
 }
